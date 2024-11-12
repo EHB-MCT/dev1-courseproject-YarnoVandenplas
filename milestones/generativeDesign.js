c7eaa42e2edee8a3a3b35344da305ac7850c1862
Yarno Vandenplas <yarno.vandenplas@student.ehb.be>
2024-11-12 20:52:22 +0100

    milestone2 background

--- milestones/generativeDesign.js
+++ milestones/generativeDesign.js
@@ -5,3 +5,19 @@ import * as noise from "../scripts/noise.js";
 
 let width = context.canvas.width;
 let height = context.canvas.height;
+let r = Utils.randomNumber(0, 255);
+
+//background
+ground();
+
+function ground() {
+    context.fillStyle = "green";
+    context.fillRect(0, 0, width, height);
+}
+
+sky();
+
+function sky() {
+    context.fillStyle = Utils.rgb(0, 0, r);
+    context.fillRect(0, 0, width, height - height / 4);
+}
