1b6bdaefe8175a237313516aad195d680fa6e6cb
Yarno Vandenplas <yarno.vandenplas@student.ehb.be>
2024-11-12 17:10:25 +0100

    Startup

---
+++ milestones/generativeDesign.js
@@ -0,0 +1,7 @@
+"use strict";
+import context from "../scripts/context.js";
+import * as Utils from "../scripts/utils.js";
+import * as noise from "../scripts/noise.js";
+
+let width = context.canvas.width;
+let height = context.canvas.height;
