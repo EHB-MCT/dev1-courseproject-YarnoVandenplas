03ea7e59b6d25bc1a6fadb711351a5c672d63221
Yarno Vandenplas <yarno.vandenplas@student.ehb.be>
2024-11-13 01:48:32 +0100

    Finished

--- milestones/generativeDesign.js
+++ milestones/generativeDesign.js
@@ -7,6 +7,7 @@ let width = context.canvas.width;
 let height = context.canvas.height;
 let r = Utils.randomNumber(0, 255);
 
+
 //background
 ground();
 
@@ -20,4 +21,33 @@ sky();
 function sky() {
     context.fillStyle = Utils.rgb(0, 0, r);
     context.fillRect(0, 0, width, height - height / 4);
+}
+
+//flowers and stars
+//makes the flowers and stars
+function flowerAndStars(x, y) {
+    if (y < 3 * height / 4 - 5) {
+        context.fillStyle = "yellow";
+    }
+    else {
+        context.fillStyle = "white";
+    }
+        Utils.fillCircle(x, y, 5);
+}
+
+//calls for flowers
+for (let i = 0; i < 30; i++) {
+    flowerAndStars(Utils.randomNumber(5, width - 5),Utils.randomNumber(3 * height / 4 + 5 , height - 5));
+}
+
+//calls for stars and moon when night or sun when day
+if (r < 255 / 2) {
+    for (let i = 0; i < 50; i++) {
+        flowerAndStars(Utils.randomNumber(5, width - 5), Utils.randomNumber(5, 3 * height / 4 - 5));
+    }
+    Utils.fillCircle(60, 60, 50);
+}
+else {
+    context.fillStyle = "yellow";
+    Utils.fillCircle(width / 2, height / 3, 175);
 }
